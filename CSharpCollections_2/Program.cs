33e2bf149447c886d2defe3fd5b8c57e7456a6a8
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-03-24 15:42:57 -0300

    Introdução a SortedList

--- CSharpCollections_2/Program.cs
+++ CSharpCollections_2/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using CSharpCollections_2.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpCollections_2
 {
@@ -10,6 +8,42 @@ namespace CSharpCollections_2
     {
         public static void Main(string[] args)
         {
+            IDictionary<string, Aluno> alunos = new Dictionary<string, Aluno>();
+
+            alunos.Add("VT", new Aluno("Vanessa", 34672));
+            alunos.Add("AL", new Aluno("Ana", 5617));
+            alunos.Add("RN", new Aluno("Rafael", 17654));
+            alunos.Add("WM", new Aluno("Wanderson", 11287));
+
+            ImprimirDicionario(alunos);
+
+            RemoveAluno(alunos, "AL");
+
+            IDictionary<string, Aluno> sorted = new SortedList<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17654));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            ImprimirDicionario(sorted);
+
+            Console.ReadLine();
+        }
+
+        private static void ImprimirDicionario(IDictionary<string, Aluno> alunos)
+        {
+            Console.WriteLine();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+
+        private static void RemoveAluno(IDictionary<string, Aluno> alunos, string chave)
+        {
+            alunos.Remove(chave);
+            ImprimirDicionario(alunos);
         }
     }
 }
