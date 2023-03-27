a4d7f7f9774f39025e25f4e5f53e64c613349030
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-03-27 12:18:09 -0300

    Introdução a Jagged Arrays

---
+++ CSharpCollections_2 - JaggedArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections_2___JaggedArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[][] familias = new string[3][];
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
+
+            ImprimirFamilias(familias);
+
+            Console.ReadLine(); 
+        }
+
+        private static void ImprimirFamilias(string[][] familias)
+        {
+            foreach (var familia in familias)
+            {
+                Console.WriteLine(string.Join(", ", familia));
+            }
+        }
+    }
+}
